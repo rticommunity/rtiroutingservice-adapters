b069e9d62567bd6287130075b29402730cdf7abd
Gerardo Pardo <gerardo@rti.com>
2017-04-20 12:24:03 -0700

    First working version

---
+++ adapters/file/src/LineConversion.h
@@ -0,0 +1,75 @@
+/*****************************************************************************/
+/*         (c) Copyright, Real-Time Innovations, All rights reserved.        */
+/*                                                                           */
+/*         Permission to modify and use for internal purposes granted.       */
+/* This software is provided "as is", without warranty, express or implied.  */
+/*                                                                           */
+/*****************************************************************************/
+
+/* ========================================================================= */
+/* RTI Routing Service File Adapter                                          */
+/* ========================================================================= */
+/*                                                                           */
+/* See LineConversion.c                                                      */
+/*                                                                           */
+/* ========================================================================= */
+
+#ifndef _lineconversion_h_
+#define _lineconversion_h_
+
+#include <stdio.h>
+#include <string.h>
+#include "ndds/ndds_c.h"
+#include "routingservice/routingservice_adapter.h"
+
+/* ========================================================================= */
+/*                                                                           */
+/* Read line                                                                 */
+/*                                                                           */
+/* ========================================================================= */
+
+int RTIRS_FileAdapter_read_sample(
+    struct DDS_DynamicData * sampleOut, 
+    FILE * file,
+    char * buffer, 
+    int maxSampleSize,
+    RTI_RoutingServiceEnvironment * env);
+
+
+/* ========================================================================= */
+/*                                                                           */
+/* Write line                                                                */
+/*                                                                           */
+/* ========================================================================= */
+
+int RTIRS_FileAdapter_write_sample(
+	struct DDS_SampleInfo  * sample_info,
+    struct DDS_DynamicData * sample, 
+    FILE * file,
+    RTI_RoutingServiceEnvironment * env);
+
+/* ========================================================================= */
+/*                                                                           */
+/* Utility                                                                   */
+/*                                                                           */
+/* ========================================================================= */
+
+char * RTIRS_FileAdapter_trim(char * str);
+
+/*****************************************************************************/
+
+int RTIRS_FileAdapter_assign(
+    struct DDS_DynamicData * sample,
+    const char * field,
+    const char * value,
+    RTI_RoutingServiceEnvironment * env);
+
+/*****************************************************************************/
+
+int RTIRS_FileAdapter_write_member(
+    const struct DDS_DynamicData * sample,
+    const struct DDS_DynamicDataMemberInfo * info,
+    FILE * file,
+    RTI_RoutingServiceEnvironment * env);
+
+#endif
